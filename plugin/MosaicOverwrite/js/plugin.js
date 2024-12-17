72b02bdd1801721f0872998202624154c6714ded
proto <proto.jp+github@gmail.com>
2024-12-18 00:04:13 +0900

    基本形完成！

--- plugin/MosaicOverwrite/js/plugin.js
+++ plugin/MosaicOverwrite/js/plugin.js
@@ -7,7 +7,8 @@ eagle.onPluginCreate(async(plugin) => {
 	const fs = require('fs/promises');
 	const iconv = require('iconv-lite');
 
-	const targetFolderName = '1stOutputWebUI';
+	const targetFolderName = '1stOutputWebUI';//生成時出力フォルダ。処理終了の判定に使う。
+	const targetTags = ["nsfw","nude"]; // タグフィルタリング配列
 
 	async function processImage() {
 		// すべてのフォルダを取得
@@ -21,18 +22,18 @@ eagle.onPluginCreate(async(plugin) => {
 			return;
 		}
 
-		// ターゲットフォルダ内の画像を取得（rating: 0のもの）
+		// ターゲット画像を取得
 		const items = await eagle.item.get({ 
 			folders: [targetFolder.id],
-			rating: 1 
+			tags: targetTags
 		});
-
-		console.log(items);
 		
 		if (items.length === 0) {
 			console.log('該当する画像がありません。');
 			return;
 		}
+
+		console.log(`ターゲット画像 "${items.length}" 枚`);
 
 		// 仮想環境内のPython実行ファイルのパス
 		const pythonPath = 'D:\\ai\\automosaic_2024-08-17\\venv\\Scripts\\python.exe';
@@ -52,8 +53,8 @@ eagle.onPluginCreate(async(plugin) => {
 			await item.save();
 
 			// モザイク処理を実行
-			const args = [scriptPath, '-ssd', '-c', '0.37', filePath];
-			console.log(`モザイク処理を実行中: ${filePath}`);
+			const args = [scriptPath, '-ssd', '-c', '0.36', filePath];
+			// console.log(`モザイク処理を実行中: ${filePath}`);
 
 			try {
 				// Pythonスクリプト実行
@@ -67,37 +68,40 @@ eagle.onPluginCreate(async(plugin) => {
 					});
 				});
 
-				const decodedOutput = iconv.decode(stdout, 'shift_jis');
-				const decodedError = iconv.decode(stderr, 'shift_jis');
+				// const decodedOutput = iconv.decode(stdout, 'shift_jis');
+				// const decodedError = iconv.decode(stderr, 'shift_jis');
 				// console.log('モザイク処理の出力:', decodedOutput);
 				// console.log('モザイク処理のエラー出力:', decodedError);
-
-				// モザイク対象がない場合はスキップ
-				if (decodedOutput.includes('no detections')) {
-					console.log('モザイク対象がないため、画像を変更しません:', filePath);
-					continue;
-				}
 
 				// モザイク処理済みファイルのパスを生成
 				const mosaicFileName = path.basename(filePath, path.extname(filePath)) + '_mosaic.png';
 				const mosaicFilePath = path.join(path.dirname(filePath), mosaicFileName);
+				
+				// モザイク画像が存在しない場合はスキップ
+				try {
+					await fs.access(mosaicFilePath);
+				} catch {
+					console.log('モザイク処理済み画像が見つからないため、スキップします:', filePath);
+					continue;
+				}
 
 				// 画像を置き換え
 				await item.replaceFile(mosaicFilePath);
-				console.log('画像を正常に置き換えました。');
+				// console.log('画像を正常に置き換えました。');
 
 				// タグを追加
 				item.tags.push('Mosaic_ow');
 				await item.save();
-				console.log('タグを追加しました: Mosaic_ow');
+				// console.log('タグを追加しました: Mosaic_ow');
 
 				// 一時的な_mosaic.png画像を削除
 				await fs.unlink(mosaicFilePath);
-				console.log('一時的な _mosaic.png 画像を削除しました。');
+				// console.log('一時的な _mosaic.png 画像を削除しました。');
 			} catch (error) {
 				console.error('画像処理中にエラーが発生しました:', error);
 			}
 		}
+		console.log('すべての画像処理が完了しました。');
 	}
 
 	processImage().catch(error => {
